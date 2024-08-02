7272f12b1f798dd9e0c329f52180a2961bae2169
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2024-08-02 20:12:27 +0600

    Update menus.js

--- src/constant/menus.js
+++ src/constant/menus.js
@@ -5,17 +5,19 @@ import {
   FiUserPlus,
 } from "react-icons/fi";
 import { BsFillFuelPumpFill, BsFuelPump } from "react-icons/bs";
-import { IoBookmarkOutline } from "react-icons/io5";
+import { IoBookmarkOutline, IoHomeOutline } from "react-icons/io5";
 import { GoBriefcase } from "react-icons/go";
 import { LuFileClock } from "react-icons/lu";
 import { MdAccountCircle } from "react-icons/md";
+import { Home } from "lucide-react";
+import { BiHome, BiHomeAlt2 } from "react-icons/bi";
 
 export const menus = [
   // DASHBOARD
   {
     title: "Home",
     link: "/",
-    Icon: FiPieChart,
+    Icon: IoHomeOutline,
     childrens: [],
     show: true,
   },
